6b5e1c15f612c2a82ec7cfe536f51437e6a20f63
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-05-26 23:43:00 +0100

    Add Rule, Warscroll and WeaponProfile models

--- armyref/armies/admin.py
+++ armyref/armies/admin.py
@@ -12,3 +12,18 @@ class GameAdmin(OrderedModelAdmin):
 @admin.register(models.Phase)
 class PhaseAdmin(OrderedModelAdmin):
     list_display = (str, 'move_up_down_links')
+
+
+@admin.register(models.Warscroll)
+class WarscrollAdmin(OrderedModelAdmin):
+    list_display = (str, 'move_up_down_links')
+
+
+@admin.register(models.Rule)
+class RuleAdmin(OrderedModelAdmin):
+    list_display = (str, 'move_up_down_links')
+
+
+@admin.register(models.WeaponProfile)
+class WeaponProfileAdmin(OrderedModelAdmin):
+    list_display = (str, 'move_up_down_links')
--- armyref/armies/models.py
+++ armyref/armies/models.py
@@ -17,3 +17,41 @@ class Phase(OrderedModel):
 
     def __str__(self):
         return '{name} ({game})'.format(name=self.name, game=self.game)
+
+
+class Warscroll(OrderedModel):
+    name = models.CharField(max_length=255)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    order_with_respect_to = 'game'
+
+    def __str__(self):
+        return self.name
+
+
+class Rule(OrderedModel):
+    name = models.CharField(max_length=255)
+    wording = models.TextField(blank=True)
+    warscroll = models.ForeignKey(Warscroll, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WeaponProfile(OrderedModel):
+    MELEE = 'MELEE'
+    MISSILE = 'MISSILE'
+    TYPES = (
+        (MELEE, 'Melee'),
+        (MISSILE, 'Missile'),
+    )
+
+    name = models.CharField(max_length=255)
+    weapon_type = models.CharField(max_length=15, choices=TYPES)
+    wording = models.TextField(blank=True)
+    warscroll = models.ForeignKey(Warscroll, on_delete=models.CASCADE)
+
+    order_with_respect_to = 'warscroll'
+
+    def __str__(self):
+        return self.name
